97a0178e3fcbe5c8d0678a899387a6893fb4ba5e
pkgdemon <pkgdemonteam@gmail.com>
2024-04-12 16:41:49 -0400

    Revert a few changes for now to troubleshoot debootstrap

--- build.sh
+++ build.sh
@@ -10,20 +10,12 @@ lb_config='\
     --iso-volume "Gershwin"
     '
 
-if [ -f "/__w/iso/iso/root_amd64.zip" ]; then
-lb_config="$lb_config \\
-    --image-name gershwin-amd64-$(date +"%Y%m%d%H%m") \\
-    "
-fi
-
 if [ -f "/home/runner/work/iso/iso/root_arm64.zip" ]; then
 lb_config="$lb_config \\
-    --image-name gershwin-arm64-$(date +"%Y%m%d%H%m")
     --architectures arm64 \\
     --bootloader grub-efi \\
     --bootstrap-qemu-arch arm64 \\
     --bootstrap-qemu-static /usr/bin/qemu-arm-static \\
-    ----verbose 3 \\
     "
 fi
 
