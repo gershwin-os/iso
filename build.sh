5afad4f58b653a03bfd2a4e07dc22820a4c5fa50
pkgdemon <pkgdemonteam@gmail.com>
2024-04-12 15:06:09 -0400

    Improve lb_config

--- build.sh
+++ build.sh
@@ -1,34 +1,17 @@
 #!/bin/sh
 
-# Get the machine architecture
-arch=$(uname -m)
-
-uname -a
-uname -m
-
-# Check if it's a 64-bit ARM system
-if [ "$arch" = "aarch64" ]; then
-    export LB_BUILD_ARCH="arm64"
-# Check if it's a 64-bit Intel system
-elif [ "$arch" = "x86_64" ]; then
-    export LB_BUILD_ARCH=ARCHITECTURE="amd64"
-else
-    echo "Unsupported architecture: $arch"
-    exit 1
-fi
-
 rm -rf live-default
 mkdir live-default
 cd live-default || exit
 
 lb_config='\
     --distribution bookworm \
-    --architectures $LB_BUILD_ARCH \
     --archive-areas "main contrib non-free non-free-firmware" \
     --iso-volume "Gershwin"
     '
 if [ "$LB_BUILD_ARCH" == 'arm64' ]; then
 lb_config="$lb_config \\
+    --architectures $LB_BUILD_ARCH \
     --bootloader grub-efi \\
     --bootstrap-qemu-arch arm64 \\
     --bootstrap-qemu-static /usr/bin/qemu-arm-static \\
