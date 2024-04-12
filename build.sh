2e59204920810b06a8f0ca496ccfa144dd6d7276
pkgdemon <pkgdemonteam@gmail.com>
2024-04-11 20:33:12 -0400

    Adding initial build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+rm -rf live-default
+mkdir live-default
+cd live-default || exit
+lb config --distribution bookworm --archive-areas "main contrib non-free non-free-firmware" --iso-volume "Gershwin"
+
+echo "xorg" > config/package-lists/gershwin.list.chroot
+pwd
+#cp -R ../overlay/* config/includes.chroot_after_packages/
+
+cat <<EOF > config/hooks/live/gershwin.hook.chroot
+#!/bin/sh
+
+set -e
+
+# This script is run inside the ISO chroot after packages
+
+EOF
+
+lb build
