43508cb901eabc63fccddb8d274040248d942b9c
pkgdemon <pkgdemonteam@gmail.com>
2024-06-23 19:09:23 -0400

    Determine which archives need to be used

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ set -e
 # Initialize WORKDIR to empty (if not already set)
 export WORKDIR=""
 
+# Determine the architecture of the Linux builder
+ARCH=$(dpkg --print-architecture)
+
 # Detect whether or not GitHub actions is being used
 if [ -d "/__w/iso/iso/" ]; then
   echo "GH actions AMD64 runner detected"
@@ -50,52 +53,28 @@ lb config $lb_config
 echo "xorg" > config/package-lists/gershwin.list.chroot
 echo "git" > config/package-lists/gershwin.list.chroot
 
-# Modify package list extract GNUstep for AMD64
-if [ -d "/__w/iso/iso/" ]; then
-  cp /__w/iso/iso/config/package-lists/gershwin.list.chroot config/package-lists/gershwin.list.chroot
-  tar -xf /__w/iso/iso/system-amd64.tar.gz -C /__w/iso/iso/live-default/config/includes.chroot_after_packages/
-  tar -xf /__w/iso/iso/developer-amd64.tar.gz -C /__w/iso/iso/live-default/config/includes.chroot_after_packages/
-  tar -xf /__w/iso/iso/applications-amd64.tar.gz -C /__w/iso/iso/live-default/config/includes.chroot_after_packages/
-  ls /__w/iso/iso/live-default/config/includes.chroot_after_packages/
-  ls /__w/iso/iso/live-default/config/includes.chroot_after_packages/Applications
-  ls /__w/iso/iso/live-default/config/includes.chroot_after_packages/Developer/Applications
-  ls /__w/iso/iso/live-default/config/includes.chroot_after_packages/System/Applications
-  # Fetch and extract overlays
-  wget -O opt.zip https://github.com/gershwin-os/opt/archive/refs/heads/main.zip
-  wget -O etc.zip https://github.com/gershwin-os/etc/archive/refs/heads/main.zip
-  wget -O desktop-pictures.zip https://github.com/gershwin-os/desktop-pictures/archive/refs/heads/main.zip
-  mkdir -p /__w/iso/iso/live-default/config/includes.chroot_after_packages/etc
-  mkdir -p /__w/iso/iso/live-default/config/includes.chroot_after_packages/opt
-  mkdir -p /__w/iso/iso/live-default/config/includes.chroot_after_packages/System/Library/Desktop\ Pictures/
-  bsdtar -xf opt.zip -C /__w/iso/iso/live-default/config/includes.chroot_after_packages/opt/ --strip-components 1 --exclude 'LICENSE' --exclude 'README.md'
-  bsdtar -xf etc.zip -C /__w/iso/iso/live-default/config/includes.chroot_after_packages/etc/ --strip-components 1 --exclude 'LICENSE' --exclude 'README.md'
-  bsdtar -xf desktop-pictures.zip -C /__w/iso/iso/live-default/config/includes.chroot_after_packages/System/Library/Desktop\ Pictures/ --strip-components 1 --exclude 'README.md'
-  chmod +x /__w/iso/iso/live-default/config/includes.chroot_after_packages/opt/bin/*
-  ls /__w/iso/iso/live-default/config/includes.chroot_after_packages/opt/
-fi
+# Modify package list extract GNUstep
+cp ${WORKDIR}/config/package-lists/gershwin.list.chroot config/package-lists/gershwin.list.chroot
+tar -xf ${WORKDIR}/system-${ARCH}.tar.gz -C ${WORKDIR}/live-default/config/includes.chroot_after_packages/
+tar -xf ${WORKDIR}/developer-${ARCH}.tar.gz -C ${WORKDIR}/live-default/config/includes.chroot_after_packages/
+tar -xf ${WORKDIR}/applications-${ARCH}.tar.gz -C ${WORKDIR}/live-default/config/includes.chroot_after_packages/
+ls ${WORKDIR}/live-default/config/includes.chroot_after_packages/
+ls ${WORKDIR}/live-default/config/includes.chroot_after_packages/Applications
+ls ${WORKDIR}/live-default/config/includes.chroot_after_packages/Developer/Applications
+ls ${WORKDIR}/live-default/config/includes.chroot_after_packages/System/Applications
+# Fetch and extract overlays
+wget -O opt.zip https://github.com/gershwin-os/opt/archive/refs/heads/main.zip
+wget -O etc.zip https://github.com/gershwin-os/etc/archive/refs/heads/main.zip
+wget -O desktop-pictures.zip https://github.com/gershwin-os/desktop-pictures/archive/refs/heads/main.zip
+mkdir -p ${WORKDIR}/live-default/config/includes.chroot_after_packages/etc
+mkdir -p ${WORKDIR}/live-default/config/includes.chroot_after_packages/opt
+mkdir -p ${WORKDIR}/live-default/config/includes.chroot_after_packages/System/Library/Desktop\ Pictures/
+bsdtar -xf opt.zip -C ${WORKDIR}/live-default/config/includes.chroot_after_packages/opt/ --strip-components 1 --exclude 'LICENSE' --exclude 'README.md'
+bsdtar -xf etc.zip -C ${WORKDIR}/live-default/config/includes.chroot_after_packages/etc/ --strip-components 1 --exclude 'LICENSE' --exclude 'README.md'
+bsdtar -xf desktop-pictures.zip -C ${WORKDIR}/live-default/config/includes.chroot_after_packages/System/Library/Desktop\ Pictures/ --strip-components 1 --exclude 'README.md'
+chmod +x ${WORKDIR}/live-default/config/includes.chroot_after_packages/opt/bin/*
+ls ${WORKDIR}/live-default/config/includes.chroot_after_packages/opt/
 
-# Modify package list and extract GNUstep for ARM64
-if [ -d "/home/runner/work/iso/iso/" ]; then
-  cp /home/runner/work/iso/iso/config/package-lists/gershwin.list.chroot config/package-lists/gershwin.list.chroot
-  tar -xf /home/runner/work/iso/iso/system-arm64.tar.gz -C /home/runner/work/iso/iso/live-default/config/includes.chroot_after_packages/
-  tar -xf /home/runner/work/iso/iso/developer-arm64.tar.gz -C /home/runner/work/iso/iso/live-default/config/includes.chroot_after_packages/
-  tar -xf /home/runner/work/iso/iso/applications-arm64.tar.gz -C /home/runner/work/iso/iso/live-default/config/includes.chroot_after_packages/
-  ls /home/runner/work/iso/iso/live-default/config/includes.chroot_after_packages/
-  ls /home/runner/work/iso/iso/live-default/config/includes.chroot_after_packages/Applications
-  ls /home/runner/work/iso/iso/live-default/config/includes.chroot_after_packages/Developer/Applications
-  ls /home/runner/work/iso/iso/live-default/config/includes.chroot_after_packages/System/Applications
-  # Fetch and extract overlays
-  wget -O opt.zip https://github.com/gershwin-os/opt/archive/refs/heads/main.zip
-  wget -O etc.zip https://github.com/gershwin-os/etc/archive/refs/heads/main.zip
-  wget -O desktop-pictures.zip https://github.com/gershwin-os/desktop-pictures/archive/refs/heads/main.zip
-  mkdir -p /home/runner/work/iso/iso/live-default/config/includes.chroot_after_packages/etc
-  mkdir -p /home/runner/work/iso/iso/live-default/config/includes.chroot_after_packages/opt
-  mkdir -p /home/runner/work/iso/iso/live-default/config/includes.chroot_after_packages/System/Library/Desktop\ Pictures/
-  bsdtar -xf opt.zip -C /home/runner/work/iso/iso/live-default/config/includes.chroot_after_packages/opt/ --strip-components 1 --exclude 'LICENSE' --exclude 'README.md'
-  unzip etc.zip -C /home/runner/work/iso/iso/live-default/config/includes.chroot_after_packages/etc/ --strip-components 1 --exclude 'LICENSE' --exclude 'README.md'
-  unzip desktop-pictures.zip -C /home/runner/work/iso/iso/live-default/config/includes.chroot_after_packages/System/Library/Desktop\ Pictures/ --strip-components 1 --exclude 'README.md'
-  chmod +x /home/runner/work/iso/iso/live-default/config/includes.chroot_after_packages/opt/bin/*
-fi
 
 cat <<EOF > config/hooks/live/gershwin.hook.chroot
 #!/bin/sh
