4ee18673828343eb6f7d1fbca0d32a6232dda5d9
pkgdemon <pkgdemonteam@gmail.com>
2024-09-17 18:46:37 -0500

    Add ZFS packages

--- build.sh
+++ build.sh
@@ -42,6 +42,7 @@ cd live-default || exit
 
 lb_config=" \
     --distribution bookworm \
+    --archive-areas "main contrib non-free non-free-firmware" \
     --image-name gershwin-$(date +"%Y%m%d%H%m") \
     --iso-volume "'"Gershwin"'"
     "
